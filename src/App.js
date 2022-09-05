eed8877c6b35d38f2a8a8db6cef13db643f06683
Kamil Słomiński <kam_slomka@wp.pl>
2022-09-05 15:06:42 +0200

    Map and List component functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
 import Header from './components/Header/Header';
