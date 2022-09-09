11e7f29dfbbfbbfa4a8503f3288ef8498584d776
Kamil Słomiński <kam_slomka@wp.pl>
2022-09-09 21:24:07 +0200

    Scroll Places and Filters

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+  const [childClicked, setChildClicked] = useState(null);
+
   const [type, setType] = useState('restaurants');
 
   const [coords, setCoords] = useState({});
   const [bounds, setBounds] = useState(null);
 
-  const [places, setPlaces] = useState([]);
-
   const [isLoading, setIsLoading] = useState(false);
+
+  const [rating, setRating] = useState('');
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -26,11 +31,19 @@ const App = () => {
   }, []);
 
   useEffect(() => {
+    const filteredPlaces = places.filter((place) => place.rating > rating);
+
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
     if (bounds) {
       setIsLoading(true);
 
       getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
         setPlaces(data);
+        setFilteredPlaces([]);
+        setIsLoading(false);
       });
     }
   }, [bounds, type]);
@@ -41,14 +54,23 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List places={places} />
+          <List
+            places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
             setCoords={setCoords}
             setBounds={setBounds}
             coords={coords}
-            places={places}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
